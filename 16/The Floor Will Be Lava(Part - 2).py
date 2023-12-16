134a8f5a794fb4f4b0cb96e89eef6a0b31120542
Sai Ram Pendyala <89296568+sai-ram-pendyala@users.noreply.github.com>
2023-12-16 12:19:52 +0530

    16

--- 16/The Floor Will Be Lava(Part - 2).py
+++ 16/The Floor Will Be Lava(Part - 2).py
@@ -2,89 +2,69 @@ from collections import deque
 
 def bfs(mat, i, j, dirt):
     def isValid(i, j):
-        if min(i, j) < 0 or i >= len(mat) or j >= len(mat[0]):
-            return False
-        return True
-    
+        return 0 <= i < len(mat) and 0 <= j < len(mat[0])
+
+    def addNeighbor(i, j, dirt):
+        if isValid(i, j) and (i, j, dirt) not in vis:
+            q.append((i, j, dirt))
+            vis.add((i, j, dirt))
+
     q = deque([(i, j, dirt)])
-    vis = set((i, j, dirt))
+    vis = set()
+    vis.add((i, j, dirt))
     eng = set()
 
     while q:
         i, j, dirt = q.popleft()
         eng.add((i, j))
-    
+
         if mat[i][j] == '.':
-            if dirt == 'l' and isValid(i, j - 1) and (i, j - 1, dirt) not in vis:
-                q.append((i, j - 1, dirt))
-                vis.add(((i, j - 1, dirt)))
-            elif dirt == 'r'and isValid(i, j + 1) and (i, j + 1, dirt) not in vis:
-                q.append((i, j + 1, dirt))
-                vis.add(((i, j + 1, dirt)))
-            elif dirt == 'u' and isValid(i - 1, j) and (i - 1, j, dirt) not in vis:
-                q.append((i - 1, j, dirt))
-                vis.add(((i - 1, j, dirt)))
-            elif dirt == 'd' and isValid(i + 1, j) and (i + 1, j, dirt) not in vis:
-                q.append((i + 1, j, dirt))
-                vis.add(((i + 1, j, dirt)))
+            if dirt == 'l':
+                addNeighbor(i, j - 1, dirt)
+            elif dirt == 'r':
+                addNeighbor(i, j + 1, dirt)
+            elif dirt == 'u':
+                addNeighbor(i - 1, j, dirt)
+            elif dirt == 'd':
+                addNeighbor(i + 1, j, dirt)
 
         elif mat[i][j] == '-':
-            if dirt == 'l' and isValid(i, j - 1) and (i, j - 1, dirt) not in vis:
-                q.append((i, j - 1, dirt))
-                vis.add(((i, j - 1, dirt)))
-            elif dirt == 'r'and isValid(i, j + 1) and (i, j + 1, dirt) not in vis:
-                q.append((i, j + 1, dirt))
-                vis.add(((i, j + 1, dirt)))
+            if dirt == 'l':
+                addNeighbor(i, j - 1, dirt)
+            elif dirt == 'r':
+                addNeighbor(i, j + 1, dirt)
             else:
-                if isValid(i, j - 1) and (i, j - 1, 'l') not in vis:
-                    q.append((i, j - 1, 'l'))
-                    vis.add(((i, j - 1, 'l')))
-                if isValid(i, j + 1) and (i, j + 1, 'r') not in vis:
-                    q.append((i, j + 1, 'r'))
-                    vis.add(((i, j + 1, 'r')))
+                addNeighbor(i, j - 1, 'l')
+                addNeighbor(i, j + 1, 'r')
 
         elif mat[i][j] == '|':
-            if dirt == 'u' and isValid(i - 1, j) and (i - 1, j, dirt) not in vis:
-                q.append((i - 1, j, dirt))
-                vis.add(((i - 1, j, dirt)))
-            elif dirt == 'd' and isValid(i + 1, j) and (i + 1, j, dirt) not in vis:
-                q.append((i + 1, j, dirt))
-                vis.add(((i + 1, j, dirt)))
+            if dirt == 'u':
+                addNeighbor(i - 1, j, dirt)
+            elif dirt == 'd':
+                addNeighbor(i + 1, j, dirt)
             else:
-                if isValid(i - 1, j) and (i - 1, j, 'u') not in vis:
-                    q.append((i - 1, j, 'u'))
-                    vis.add(((i - 1, j, 'u')))
-                if isValid(i + 1, j) and (i + 1, j, 'd') not in vis:
-                    q.append((i + 1, j, 'd'))
-                    vis.add(((i + 1, j, 'd')))
+                addNeighbor(i - 1, j, 'u')
+                addNeighbor(i + 1, j, 'd')
 
         elif mat[i][j] == '/':
-            if dirt == 'l' and isValid(i + 1, j) and (i + 1, j, 'd') not in vis:
-                q.append((i + 1, j, 'd'))
-                vis.add(((i + 1, j, 'd')))
-            elif dirt == 'r' and isValid(i - 1, j) and (i - 1, j, 'u') not in vis:
-                q.append((i - 1, j, 'u'))
-                vis.add(((i - 1, j, 'u')))
-            elif dirt == 'd' and isValid(i, j - 1) and (i, j - 1, 'l') not in vis:
-                q.append((i, j - 1, 'l'))
-                vis.add(((i, j - 1, 'l')))
-            elif dirt == 'u' and isValid(i, j + 1) and (i, j + 1, 'r') not in vis:
-                q.append((i, j + 1, 'r'))
-                vis.add(((i, j + 1, 'r')))
-        
+            if dirt == 'l':
+                addNeighbor(i + 1, j, 'd')
+            elif dirt == 'r':
+                addNeighbor(i - 1, j, 'u')
+            elif dirt == 'd':
+                addNeighbor(i, j - 1, 'l')
+            else:
+                addNeighbor(i, j + 1, 'r')
+
         else:
-            if dirt == 'l' and isValid(i - 1, j) and (i - 1, j, 'u') not in vis:
-                q.append((i - 1, j, 'u'))
-                vis.add(((i - 1, j, 'u')))
-            elif dirt == 'r' and isValid(i + 1, j) and (i + 1, j, 'd') not in vis:
-                q.append((i + 1, j, 'd'))
-                vis.add(((i + 1, j, 'd')))
-            elif dirt == 'd' and isValid(i, j + 1) and (i, j + 1, 'r') not in vis:
-                q.append((i, j + 1, 'r'))
-                vis.add(((i, j + 1, 'r')))
-            elif dirt == 'u' and isValid(i, j - 1) and (i, j - 1, 'l') not in vis:
-                q.append((i, j - 1, 'l'))
-                vis.add(((i, j - 1, 'l')))
+            if dirt == 'l':
+                addNeighbor(i - 1, j, 'u')
+            elif dirt == 'r':
+                addNeighbor(i + 1, j, 'd')
+            elif dirt == 'd':
+                addNeighbor(i, j + 1, 'r')
+            else:
+                addNeighbor(i, j - 1, 'l')
 
     return len(eng)
 
